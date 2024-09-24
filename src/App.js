cee17931fa7b77c5e522ddb609d6a0d4d9c5ca79
innalytics34 <arun@innalytics.in>
2024-09-24 11:09:11 +0530

    initial commits

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+import './dx-styles.scss';
+import LoadPanel from 'devextreme-react/load-panel';
+import { NavigationProvider } from './contexts/navigation';
+import { AuthProvider, useAuth } from './contexts/auth';
+import { useScreenSizeClass } from './utils/media-query';
+import Content from './Content';
+import UnauthenticatedContent from './UnauthenticatedContent';
+import 'devexpress-gantt/dist/dx-gantt.css';
+import { useThemeContext, ThemeContext } from './themes/theme';
+
+function App() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <LoadPanel visible={true} />;
+  }
+
+  if (user) {
+    return <Content />;
+  }
+
+  return <UnauthenticatedContent />;
+}
+
+export default function Root() {
+  const screenSizeClass = useScreenSizeClass();
+    const themeContext = useThemeContext();
+
+  return (
+    <Router>
+      <ThemeContext.Provider value={themeContext}>
+      <AuthProvider>
+        <NavigationProvider>
+          <div className={`app ${screenSizeClass}`}>
+            <App />
+          </div>
+        </NavigationProvider>
+      </AuthProvider>
+      </ThemeContext.Provider>
+    </Router>
+  );
+}
---
+++ src/app-info.js
@@ -0,0 +1,5 @@
+const appInfo = {
+    title: 'Report'
+};
+export default appInfo;
+
